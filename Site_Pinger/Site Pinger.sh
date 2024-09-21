cba9cdb94a53b11f1ede8b391da14829e261208b
oziesiek <email.pawelorzechowski@gmail.com>
2024-09-21 13:05:19 +0200

    fixed version

--- Site_Pinger/Site Pinger.sh
+++ Site_Pinger/Site Pinger.sh
@@ -1,40 +1,29 @@
 #!/bin/bash
 
-# Get the number of repetitions from the user
-read -p "Enter the number of repetitions: " repetitions
+# Get the number of URLs from the user
+read -p "How many URLs do you want to ping? " url_count
+urls=()
 
-# Get the time interval from the user (in seconds)
+# Loop to collect URLs
+for (( i=1; i<=url_count; i++ ))
+do
+    read -p "Enter URL #$i: " url
+    urls+=("$url")
+done
+
+# Get the interval time from the user
 read -p "Enter the time interval (in seconds): " interval
 
-# URLs to check
-url1="https://www.olx.pl/d/oferta/uchwyt-do-monitora-vesa-100x100-bardzo-dobry-stan-CID99-ID122gAM.html?bs=olx_pro_listing"
-url2="https://www.olx.pl/d/oferta/drazek-multi-grip-do-sufitu-sciany-w-dobrym-stanie-okazyjna-cena-CID767-ID122g0W.html?bs=olx_pro_listing"
+# Calculate total time for execution
+total_time=$((url_count * interval))
+echo "Total execution time will be approximately $total_time seconds."
 
-# Calculate total execution time
-total_seconds=$((interval * (repetitions - 1)))
-
-# Convert seconds to minutes and seconds
-minutes=$((total_seconds / 60))
-seconds=$((total_seconds % 60))
-
-# Display the estimated total execution time
-if [ $minutes -gt 0 ]; then
-    echo "The script will take approximately $minutes minutes and $seconds seconds to complete."
-else
-    echo "The script will take approximately $seconds seconds to complete."
-fi
-
-# Perform the HTTP request the given number of times at specified intervals
-for ((i=1; i<=repetitions; i++))
+# Ping each URL at the specified interval
+for url in "${urls[@]}"
 do
-    echo "Request $i to $url1"
-    curl -s -o /dev/null -w "%{http_code}\n" "$url1"  # Make a request to URL1 and display the HTTP status code
-
-    echo "Request $i to $url2"
-    curl -s -o /dev/null -w "%{http_code}\n" "$url2"  # Make a request to URL2 and display the HTTP status code
-
-    # Wait for the specified interval if this is not the last iteration
-    if [ $i -lt $repetitions ]; then
-        sleep $interval
-    fi
+    while true; do
+        echo "Pinging $url..."
+        curl -s -o /dev/null -w "HTTP Response Code: %{http_code}\n" "$url"
+        sleep "$interval"
+    done
 done
