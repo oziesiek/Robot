1b5ee3e11522e41668a778c0dd2472bd2044bd40
oziesiek <email.pawelorzechowski@gmail.com>
2024-10-10 22:35:45 +0200

    duzy change

--- Safe_VPN_Server/run_me.ps1
+++ Safe_VPN_Server/run_me.ps1
@@ -2,33 +2,40 @@
 
 # Step 1: Prompt user for necessary inputs
 $SERVER_PUBLIC_IP = Read-Host "Please enter the Server's Public IP"
+$KEY_NAME = Read-Host "Please enter a name for your VPN key file (without extension):"
 
 # Step 2: Function to generate SSH keys
 function Generate-SSHKeys {
-    Write-Host "Generating SSH keys..."
-    ssh-keygen -t rsa -f $HOME\.ssh\vpn2 -N ""  # No passphrase for the key
-    Write-Host "SSH Keys generated!"
+    param (
+        [string]$KeyName  # Accept the key name as a parameter
+    )
+    
+    Write-Host "Generating SSH keys..." -ForegroundColor Yellow
+    ssh-keygen -t rsa -f "$HOME\.ssh\$KEY_NAME" "" *>&1 | Out-Null  # Suppress output
+    Write-Host "SSH Keys generated!" -ForegroundColor Yellow
 }
 
 # Step 3: Function to transfer the public key and bash script to the server
 function Transfer-Files {
-    Write-Host "Transferring SSH public key to the server..."
-    scp "$HOME\.ssh\vpn2.pub" root@{$SERVER_PUBLIC_IP}:~/.ssh/authorized_keys
+    Write-Host "Transferring SSH public key to the server..." -ForegroundColor Blue
+    scp "$HOME\.ssh\$KEY_NAME.pub" root@{$SERVER_PUBLIC_IP}:~/.ssh/authorized_keys
     Write-Host "Public key transferred!"
 
     # Transfer the bash script to the server
     $bashScript = "vpn-setup.sh" 
-    Write-Host "Transferring VPN setup script to the server..."
+    Write-Host "Transferring VPN setup script to the server..." -ForegroundColor Blue
     scp $bashScript root@{$SERVER_PUBLIC_IP}:/root/vpn-setup.sh
     Write-Host "VPN setup script transferred!"
 }
-Generate-SSHKeys
+
+# Call the function with the key name
+Generate-SSHKeys -KeyName $KEY_NAME
 Transfer-Files
 
 # Final output
-Write-Host "Your SSH keys have been generated and transferred along with the setup script to the server."
-Write-Host "Please use the following command to connect to the server and run the script:"
-Write-Host "ssh -i $HOME\.ssh\vpn2 root@$SERVER_PUBLIC_IP"
+Write-Host "Your SSH keys have been generated and transferred along with the setup script to the server." -ForegroundColor Green
+Write-Host "Please use the following command to connect to the server and run the vpn-setup.sh:" 
+Write-Host "ssh -i $HOME\.ssh\$KEY_NAME root@$SERVER_PUBLIC_IP" -ForegroundColor Red
 
 
 
